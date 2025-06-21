2e3d7a097b5ef89136309cd5e453f895bef2644c
LeoBorborema <borborema.leo@yahoo.com>
2025-06-21 14:17:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def convert():
         return f"Erro na conversão: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
